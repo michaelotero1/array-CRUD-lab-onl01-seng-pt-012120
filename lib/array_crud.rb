29fde670a3b569340f9d1a2c96be876883dbf736
michael otero <michaelotero1118@gmail.com>
2020-01-31 18:41:41 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,5 +1,5 @@
-def create_an_empty_array
-  
+def create_an_empty_array = []
+  puts []
 end
 
 def create_an_array
@@ -7,6 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
+  
   
 end
 
